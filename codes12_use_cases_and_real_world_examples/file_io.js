b3655e9be14a140c650023bb49759b87da027e81
Eng. Elias <eng.elias.owis@gmail.com>
2023-09-23 11:34:36 +0300

    Revert "rename folders for ordering"
    This reverts commit 675fee1769642c97cb550fdc603c8d7d6520e598.

--- codes12_use_cases_and_real_world_examples/file_io.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs").promises;
-
-fs.readFile("file.txt", "utf8")
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
